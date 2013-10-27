0558e4204661f8db9610835d421a85f26840d4ac
Tomislav Maric <tomislav.maric@gmx.com>
2013-10-27 13:28:47 +0100

    Registering the front to runTime.

--- applications/solvers/lentReconstruct/lentReconstruct.C
+++ applications/solvers/lentReconstruct/lentReconstruct.C
@@ -72,7 +72,7 @@ int main(int argc, char *argv[])
         IOobject(
             "front.stl",
             "front",
-            mesh, 
+            runTime, 
             IOobject::MUST_READ, 
             IOobject::AUTO_WRITE
         )
