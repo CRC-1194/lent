07b794c4792615ad032502ad5e381a47bb93c44d
Tomislav Maric <tomislav.maric@gmx.com>
2014-01-15 13:52:58 +0100

    LENT-20 Added testing functions and testAdvection.

--- applications/testing/testLentAdvection/testLentAdvection.C
+++ applications/testing/testLentAdvection/testLentAdvection.C
@@ -22,19 +22,26 @@ License
     along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
 
 Application
-    levelSetFrontFoam
+    lentAdvect
 
 Authors
-    Tomislav Maric maric@csi.tu-darmstadt.de, tomislav@sourceflux.de 
+    Tomislav Maric
+    maric<<at>>csi<<dot>>tu<<minus>>darmstadt<<dot>>de
+    tomislav<<dot>>maric<<at>>gmx<<dot>>com
 
 Description
-    Test application for the interface reconstruction algorithm of the LENT method.
+    Test application for the interface advection algorithm of the LENT method.  
 
 \*---------------------------------------------------------------------------*/
 
 #include "fvCFD.H"
+#include "MULES.H"
+#include "subCycle.H"
 #include "interfaceProperties.H"
 #include "incompressibleTwoPhaseMixture.H"
+#include "turbulenceModel.H"
+#include "pimpleControl.H"
+#include "fvIOoptionList.H"
 #include "lentMethod.H"
 #include "lentTests.H"
 
@@ -42,43 +49,76 @@ Description
 
 using namespace FrontTracking;
 
-
 int main(int argc, char *argv[])
 {
     #include "setRootCase.H"
     #include "createTime.H"
     #include "createMesh.H"
 
+    #include "initContinuityErrs.H"
     #include "createFields.H"
+    #include "readTimeControls.H"
+    #include "CourantNo.H"
+    #include "setInitialDeltaT.H"
+
 
     // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+    Info<< "\nStarting time loop\n" << endl;
 
     triSurfaceFront front(
         IOobject(
             "front.stl",
             "front",
             runTime, 
-            IOobject::MUST_READ, 
+            IOobject::NO_READ, 
             IOobject::AUTO_WRITE
         )
     );
 
+    triSurfaceFrontGeoMesh frontMesh(front); 
+
+    triSurfaceFrontVectorField frontVelocity(
+        IOobject(
+            "frontVelocity", 
+            runTime.timeName(), 
+            runTime, 
+            IOobject::NO_READ,
+            IOobject::NO_WRITE
+        ),
+        front, 
+        dimensionedVector(
+            "zero",
+            dimLength / dimTime, 
+            vector(0,0,0)
+        )
+    ); 
+
     lentMethod lent(front, mesh); 
 
     lent.calcSearchDistances(searchDistanceSqr, pointSearchDistanceSqr);
-    
+
     lent.reconstructFront(front, signedDistance, pointSignedDistance); 
 
+    Test::testTriSurfaceNormalConsistency(front); 
+    
     front.write(); 
 
-    while (runTime.run()) {
+    while (runTime.run())
+    {
+        #include "readTimeControls.H"
 
         runTime++;
+
+        #include "CourantNo.H"
+        #include "heavisideCourantNo.H"
+        #include "setDeltaT.H"
 
         Info<< "Time = " << runTime.timeName() << nl << endl;
 
         twoPhaseProperties.correct();
 
+        Pout << "Signed distances..."; 
         lent.calcSignedDistances(
             signedDistance, 
             pointSignedDistance,
@@ -86,13 +126,26 @@ int main(int argc, char *argv[])
             pointSearchDistanceSqr, 
             front
         ); 
+        Pout << "done." << endl;
 
+        Pout << "Heaviside ... "; 
         lent.calcHeaviside(heaviside, signedDistance, searchDistanceSqr); 
+        Pout << "done." << endl;
 
+        Pout << "Reconstruction ..."; 
         lent.reconstructFront(front, signedDistance, pointSignedDistance); 
+        Pout << "done." << endl;
 
         Test::testTriSurfaceNormalConsistency(front); 
 
+        Pout << "Velocity ..."; 
+        lent.calcFrontVelocity(frontVelocity, U); 
+        Pout << "done." << endl;
+
+        Pout << "Evolution ...";
+        lent.evolveFront(front, frontVelocity); 
+        Pout << "done." << endl;
+        
         runTime.write();
 
         Info<< "ExecutionTime = " << runTime.elapsedCpuTime() << " s"
