30ec1abbf3bb9029b524ed1ffaba431c885a8bf0
Tomislav Maric <tomislav.maric@gmx.com>
2014-10-24 12:55:22 +0200

    Unified sphere curvature model.

---
+++ src/frontCurvatureModels/frontExactSphereCurvatureModel/frontExactSphereCurvatureModel.C
@@ -0,0 +1,176 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     | Version:  2.2.x                               
+    \\  /    A nd           | Copyright held by original author
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software; you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by the
+    Free Software Foundation; either version 2 of the License, or (at your
+    option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM; if not, write to the Free Software Foundation,
+    Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+
+Class
+    Foam::frontExactSphereCurvatureModel
+
+SourceFiles
+    frontExactSphereCurvatureModel.C
+
+Author
+    Tomislav Maric maric@csi.tu-darmstadt.de
+
+Description
+
+    Curvature model used for validating the pressure-velocity coupling algorithm. 
+    It reads an exact curvature field.
+
+    You may refer to this software as :
+    //- full bibliographic data to be provided
+
+    This code has been developed by :
+        Tomislav Maric maric@csi.tu-darmstadt.de (main developer)
+    under the project supervision of :
+        Holger Marschall <marschall@csi.tu-darmstadt.de> (group leader).
+    
+    Method Development and Intellectual Property :
+    	Tomislav Maric maric@csi.tu-darmstadt.de
+    	Holger Marschall <marschall@csi.tu-darmstadt.de>
+    	Dieter Bothe <bothe@csi.tu-darmstadt.de>
+
+        Mathematical Modeling and Analysis
+        Center of Smart Interfaces
+        Technische Universitaet Darmstadt
+       
+    If you use this software for your scientific work or your publications,
+    please don't forget to acknowledge explicitly the use of it.
+
+\*---------------------------------------------------------------------------*/
+
+#include "frontExactSphereCurvatureModel.H"
+#include "addToRunTimeSelectionTable.H"
+#include "fvcAverage.H"
+#include "fvcDiv.H"
+#include "fvcGrad.H"
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+namespace Foam {
+namespace FrontTracking {
+
+    defineTypeNameAndDebug(frontExactSphereCurvatureModel, 0);
+    addToRunTimeSelectionTable(frontCurvatureModel, frontExactSphereCurvatureModel, Dictionary);
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+frontExactSphereCurvatureModel::frontExactSphereCurvatureModel(const dictionary& configDict, const Time& runTime)
+    :
+        frontCurvatureModel(configDict, runTime), 
+        center_(configDict.lookup("center")),
+        write_(configDict.lookupOrDefault<Switch>("writeExactCurvature", "no"))
+{
+    // Calculate cell curvature to write the field out in case write_ is set for the 
+    // first time-step. TM.
+    cellCurvature(); 
+}
+
+// * * * * * * * * * * * * * * * * Destructor  * * * * * * * * * * * * * * * //
+ 
+frontExactSphereCurvatureModel::~frontExactSphereCurvatureModel() {} 
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+scalar frontExactSphereCurvatureModel::calcCurvature(
+    const point& p, 
+    const point& center 
+) const
+{
+    scalar curvature = 0;
+
+    scalar radius = mag(center_ - p); 
+
+    if (radius > SMALL) 
+    {
+        curvature = 1 / radius; 
+    }
+    else
+    {
+        curvature = GREAT; 
+    }
+
+    return curvature;
+}
+
+tmp<volScalarField> frontExactSphereCurvatureModel::cellCurvature() const
+{
+    tmp<volScalarField> cellCurvatureTmp( 
+        new volScalarField(
+            IOobject(
+                "cellCurvatureExact", 
+                time().timeName(), 
+                mesh(),
+                IOobject::NO_READ,
+                IOobject::AUTO_WRITE
+            ),
+            mesh(),
+            dimensionedScalar(
+                "zero", 
+                pow(dimLength, -1), 
+                0
+            ) 
+        )
+    );
+
+    volScalarField& cellCurvature = cellCurvatureTmp(); 
+
+    const volVectorField& C = mesh().C(); 
+    const surfaceVectorField& Cf = mesh().Cf(); 
+
+    // Set internal cell centered curvature field.
+    forAll(cellCurvature, I)
+    {
+        cellCurvature[I] = calcCurvature(C[I], center_);
+    }
+
+    // Set the boundary cell centered curvature field
+    forAll(cellCurvature.boundaryField(), I)
+    {
+        fvPatchScalarField& cellCurvatureBoundary = cellCurvature.boundaryField()[I];
+        const fvsPatchVectorField& CfBoundary = Cf.boundaryField()[I];
+
+        forAll(cellCurvatureBoundary, J)
+        {
+            cellCurvatureBoundary[J] = calcCurvature(CfBoundary[J], center_);
+        }
+    }
+
+    if (write_)
+    {
+        cellCurvature.write(); 
+    }
+
+    return cellCurvatureTmp;
+}
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+} // End namespace FrontTracking
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+} // End namespace Foam
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+// ************************************************************************* //
