7231f9a15ba1cbd2b7482f854b788e79d8433dea
bt <bt@lefou-familie.org>
2015-02-06 16:57:15 +0100

    Completed first implementation

--- applications/testing/lentTestCurvatureNormal/lentTestCurvatureNormal.C
+++ applications/testing/lentTestCurvatureNormal/lentTestCurvatureNormal.C
@@ -62,8 +62,21 @@ Description
 //using namespace FrontTracking;
 //using namespace Test;
 
+// Aux function for angle calculation
+scalar getAngle(vector a, vector b)
+{
+    return Foam::acos((a & b) / (mag(a) * mag(b)));
+}
+
+scalar cot(scalar angle)
+{
+    return 1/Foam::tan(angle);
+}
+
 void curvatureNormals(triSurfaceVectorField& cn, const triSurface& front)
 {
+    scalar pi = 52163 / 16604;
+
     cn = dimensionedVector("zero",
                             dimless/dimLength,
                             vector(0,0,0)
@@ -95,61 +108,68 @@ void curvatureNormals(triSurfaceVectorField& cn, const triSurface& front)
         // Sum up mixed area and contributions to mean curvature normal
         forAll(oneRingNeighborhood, T)
         {
-            // === Get other two vertices of T ===
+            // Compute area contributions
             // First, resolve label T to get the actual face
             labelledTri currentTri = globalFaces[T];
 
             // Get labels of the two other vertices
-            label Ql = currentTri.fcIndex(V);
-            label Rl = currentTri.rcIndex(V);
+            label Ql = currentTri.fcIndex(Vl);
+            label Rl = currentTri.rcIndex(Vl);
 
             // Resolve labels to actuals points
-            V = globalPoints[Vl];
-            Q = globalPoints[Ql];
-            R = globalPoints[Rl];
+            point V = globalPoints[Vl];
+            point Q = globalPoints[Ql];
+            point R = globalPoints[Rl];
+
+            // Edge vectors
+            vector QV = V - Q;
+            vector RV = V - R;
+            vector QR = R - Q;
+
+            // Get angles
+            scalar Va = getAngle(QV, RV);
+            scalar Qa = getAngle(QV, QR);
+            scalar Ra = pi - (Va + Qa);
+
+            // Check if obtuse in order to use the correct area metric
+            if (Va < pi/2 && Qa < pi/2 && Ra < pi/2)
+            {
+                // Use Voronoi-area
+                // Cotangent function 'cot' has to be defined locally since
+                // it is not offered by OpenFOAM
+                Amix += 1/8*(magSqr(RV)*cot(Qa) + magSqr(QV)*cot(Ra));    
+            }
+            else if (Va >= pi/2)
+            {
+                // Obtuse angle at V, use half area
+                Amix += 0.5*mag(QV ^ RV);
+            }
+            else
+            {
+                Amix += 0.25*mag(QV ^ RV);
+            }
+
+            if (Va < 0.01 || Qa < 0.01 || Ra < 0.01)
+            {
+                Info << "Angle smaller than 0.01rad\n" << endl;
+            }  
+
+            // Compute mean curvature normal contributions
+            cn[Vl] = cot(Qa)*RV + cot(Ra)*QV;
+        }
+
+        // Compute mean curvature normal for vertex V
+        // This should not be necessary...
+        if (Amix < SMALL)
+        {
+            cn[Vl] = cn[Vl]/(2*SMALL);
+        }
+        else
+        {
+            cn[Vl] = cn[Vl]/(2*Amix);
         }
     }
-
-    Info << "Avarage distance from center: " << distance / counter << endl;
 }
-
-/*
-TEST_F(lentTests, lentReconstruction)
-{
-    extern int mainArgc;
-    extern char** mainArgv;
-
-    int argc = mainArgc;
-    char** argv = mainArgv;
-
-
-    // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
-
-
-    lentMethod lent(front, mesh);
-
-    lent.calcSearchDistances(searchDistanceSqr, pointSearchDistanceSqr);
-
-    lent.reconstructFront(front, signedDistance, pointSignedDistance);
-
-    front.write();
-
-    while (runTime.run()) {
-
-        runTime++;
-
-        Info<< "Time = " << runTime.timeName() << nl << endl;
-
-        runTime.write();
-
-        Info<< "ExecutionTime = " << runTime.elapsedCpuTime() << " s"
-            << "  ClockTime = " << runTime.elapsedClockTime() << " s"
-            << nl << endl;
-    }
-
-    Info<< "End\n" << endl;
-}
-*/
 
 int main(int argc, char *argv[])
 {
@@ -187,6 +207,24 @@ int main(int argc, char *argv[])
     // Finally call the function
     curvatureNormals(cn, front);
 
+    // Check for plausability and debugging...
+    scalar counter,radius = 0;
+    forAll(cn, V)
+    {
+        if (mag(cn[V]) < 1000)
+        {
+            radius += mag(cn[V]);
+            counter++;
+        }
+        else
+        {
+            Info << "Mean curvature is: " << mag(cn[V]) << endl;
+        }
+    }
+
+    radius = radius / counter;
+
+    Info << "Average radius is : " << radius << endl;
 
     return 0;
 }
