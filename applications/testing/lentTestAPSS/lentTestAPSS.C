86edc5e6b38c6e692d32f78c43536e81c10a0806
bt <bt@lefou-familie.org>
2015-06-28 23:04:38 +0200

    Finished barycentric weighted vertex normals

--- applications/testing/lentTestAPSS/lentTestAPSS.C
+++ applications/testing/lentTestAPSS/lentTestAPSS.C
@@ -25,20 +25,137 @@ Application
     lentTestAPSS
 
 Description
+    For now: store normals and curvature in separate fields
 
 \*---------------------------------------------------------------------------*/
 
 #include "fvCFD.H"
+#include "triSurfaceFields.H"
+#include "interfaceProperties.H"
+#include "immiscibleIncompressibleTwoPhaseMixture.H"
+#include "turbulenceModel.H"
+#include "pimpleControl.H"
+
+#include "lentMethod.H"
+//#include "frontCurvatureMeyer.H"
+
+#include <fstream>
+
+#include "auxFunctions/auxFunctions.H"
 
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
-// Main program:
+// TODO: include error metrics from curvatureNormal
 
+// Aux functions
+
+
+// Compute normal at a front vertex using barycentric area weighted triangle
+// normals
+void computeFrontVertexNormals(triSurfacePointVectorField& normals,
+                               const triSurface& front)
+{
+    normals = vector(0.0, 0.0, 0.0);
+                
+    // Get label list of all mesh vertices
+    const labelList& vertices = front.meshPoints();
+
+    // Get assignment point -> faces
+    const labelListList& adjacentFaces = front.pointFaces();
+
+    // List of local point references for current patch
+    const pointField& localPoints = front.localPoints();
+
+    // List of local faces references
+    const List<labelledTri>& localFaces = front.localFaces();
+
+    forAll(vertices, Vl)
+    {
+        const labelList& oneRingNeighborhood = adjacentFaces[Vl];
+        scalar area = 0.0;
+
+        forAll(oneRingNeighborhood, T)
+        {
+            label triLabel = oneRingNeighborhood[T];
+            labelledTri currentTri = localFaces[triLabel];
+
+            // Resemble point notion from lentFoam paper
+            point VT0 = localPoints[currentTri[0]];
+            point VT1 = localPoints[currentTri[1]];
+            point VT2 = localPoints[currentTri[2]];
+
+            // Compute area normal according to lentFoam paper
+            vector areaNormal = (VT1 - VT0) ^ (VT2 - VT0);
+
+            normals[Vl] += areaNormal;
+            area += mag(areaNormal);
+        }
+
+        normals[Vl] = normals[Vl] / area;
+    }  
+}
+
+/******************************************************************************\
+ Actual method from paper "Algebraic point set surfaces"
+\******************************************************************************/
+void computeCurvature(triSurfacePointScalarField& curvature,
+                      const triSurface& front,
+                      triSurfacePointVectorField& normals)
+{
+    curvature = dimensionedScalar("zero",
+                                  dimless/dimlength,
+                                  0.0
+                                 );
+                
+    // Get label list of all mesh vertices
+    const labelList& vertices = front.meshPoints();
+
+    // Get assignment point -> faces
+    const labelListList& adjacentFaces = front.pointFaces();
+
+    // List of local point references for current patch
+    const pointField& localPoints = front.localPoints();
+
+    // List of local faces references
+    const List<labelledTri>& localFaces = front.localFaces();
+
+    forAll(vertices, Vl)
+    {
+
+    }
+
+}
+
+// Weighting function from APSS; remember that the argument x is the 
+// distance squared divided by h squared
+scalar weight(scalar x2byh2)
+{
+    scalar result = (1.0 - x2byh2)*(1.0 - x2byh2)*(1.0 - x2byh2)*(1.0 - x2byh2);
+
+    if (result < 0) {result = 0};
+
+    return result;
+}
+
+// Computation of the feature support size h; the most viable approach is not
+// clear yet. h should be chosen so sufficient points are included to
+// achieve a little smoothing/averaging without erasing features
+// A relation to the cell size of the Eulerian grid or the one/two ring
+// neighborhood seem reasonable starting points
+scalar supportSize(...)
+{
+    scalar h = 0;
+
+    return h;
+}
+
+// Main program:
 int main(int argc, char *argv[])
 {
     #include "setRootCase.H"
     #include "createTime.H"
 
     // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+    
 
     Info<< "\nEnd\n" << endl;
     return 0;
