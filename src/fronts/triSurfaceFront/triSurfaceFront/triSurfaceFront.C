a98edfa6185186f3d5f4d66e799d94693009c024
Tomislav Maric <tomislav.maric@gmx.com>
2016-04-07 17:22:40 +0200

    triSurfaceFront
    operator=(triSurface&)

--- src/fronts/triSurfaceFront/triSurfaceFront/triSurfaceFront.C
+++ src/fronts/triSurfaceFront/triSurfaceFront/triSurfaceFront.C
@@ -164,10 +164,10 @@ bool triSurfaceFront::writeObject
 
 // * * * * * * * * * * * * * * Member Operators * * * * * * * * * * * * * * //
 
-void triSurfaceFront::operator=(const isoSurface& rhs)
+void triSurfaceFront::operator=(const triSurface& rhs)
 {
 
-    static_cast<triSurface*>(this)->operator=(static_cast<const triSurface&> (rhs));
+    static_cast<triSurface*>(this)->operator=(rhs);
 }
 
 // ************************************************************************* //
