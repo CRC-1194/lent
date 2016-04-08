6af36282af097940732ab442d823674dfc3423fa
Tomislav Maric <tomislav.maric@gmx.com>
2016-04-08 13:57:28 +0200

    distance normal consistency
    Added a model class

---
+++ src/frontReconstructors/normalConsistencyAlgorithms/distanceNormalConsistency/distanceNormalConsistency.C
@@ -0,0 +1,125 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     | Version:  2.2.x                               
+    \\  /    A nd           | Copyright held by original author
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software; you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by the
+    Free Software Foundation; either version 2 of the License, or (at your
+    option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM; if not, write to the Free Software Foundation,
+    Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+
+Class
+    Foam::diffuseInterfaceProperties
+
+SourceFiles
+    diffuseInterfaceProperties.C
+
+Author
+    Tomislav Maric maric@csi.tu-darmstadt.de, tomislav@sourceflux.de
+    Mathematical Modeling and Analysis
+    Center of Smart Interfaces, TU Darmstadt
+
+Description
+    
+    You may refer to this software as :
+    //- full bibliographic data to be provided
+
+    This code has been developed by :
+        Tomislav Maric maric@csi.tu-darmstadt.de, tomislav@sourceflux.de (main developer)
+    under the project supervision of :
+        Holger Marschall <marschall@csi.tu-darmstadt.de> (group leader).
+    
+    Method Development and Intellectual Property :
+    	Tomislav Maric maric@csi.tu-darmstadt.de, tomislav@sourceflux.de
+    	Holger Marschall <marschall@csi.tu-darmstadt.de>
+    	Dieter Bothe <bothe@csi.tu-darmstadt.de>
+
+        Mathematical Modeling and Analysis
+        Center of Smart Interfaces
+        Technische Universitaet Darmstadt
+       
+    If you use this software for your scientific work or your publications,
+    please don't forget to acknowledge explicitly the use of it.
+
+\*---------------------------------------------------------------------------*/
+
+#include "distanceNormalConsistency.H"
+#include "dictionary.H"
+#include "addToRunTimeSelectionTable.H"
+#include "lentCommunication.H"
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+namespace Foam {
+namespace FrontTracking {
+
+    defineTypeNameAndDebug(distanceNormalConsistency, 0);
+    addToRunTimeSelectionTable(normalConsistency, distanceNormalConsistency, Dictionary);
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+distanceNormalConsistency::distanceNormalConsistency(const dictionary& configDict)
+    :
+        normalConsistency(configDict)
+{}
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+void distanceNormalConsistency::makeFrontNormalsConsistent(
+    triSurfaceFront& front,
+    const volScalarField& signedDistance,
+    const pointScalarField& pointSignedDistance // Not used by this algorithm.
+) const
+{
+    List<labelledTri>& triangles = static_cast<List<labelledTri>& > (front);
+    const vectorField& triangleNormals = front.faceNormals();
+    const pointField& frontPoints = front.points();
+    const fvMesh& mesh = signedDistance.mesh();
+    const pointField& cellCenters = mesh.C();
+
+    const lentCommunication& communication = 
+        mesh.lookupObject<lentCommunication>(
+            lentCommunication::registeredName(front,mesh)
+    ); 
+    const auto& triangleToCell = communication.triangleToCell();  
+
+    // For all faces
+    forAll (triangles, triangleI)
+    {
+        point P = frontPoints[triangles[triangleI][0]];
+        label cellI = triangleToCell[triangleI];
+        const point& C = cellCenters[cellI];
+        const vector& n = triangleNormals[triangleI];
+
+        if (((C - P) & n) * signedDistance[cellI] <  0)
+        {
+            triangles[triangleI].flip();
+        }
+    }
+}
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+} // End namespace FrontTracking
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+} // End namespace Foam
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+// ************************************************************************* //
