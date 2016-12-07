2823d761726f4d2410496768cff1731f102b444d
bt <bt@lefou-familie.org>
2016-12-07 15:13:52 +0100

    ENH: Updated several parts of the solver lentFoam
    Disabled the implicit update of the velocity and use the explicit reconstruction as in interFoam for now as there where no significant differences in the velocity field. Adapted the lentFoam structure to the latest interFoam version (e.g. make use of the centralized functionality provided by CorrectPhi.H). Removed some headers not needed.

--- applications/solvers/lentFoam/lentFoam.C
+++ applications/solvers/lentFoam/lentFoam.C
@@ -53,16 +53,12 @@ Description
 
 
 #include "fvCFD.H"
-//#include "CMULES.H"
 #include "EulerDdtScheme.H"
-#include "localEulerDdtScheme.H"
-#include "CrankNicolsonDdtScheme.H"
-#include "subCycle.H"
 #include "immiscibleIncompressibleTwoPhaseMixture.H"
 #include "turbulentTransportModel.H"
 #include "pimpleControl.H"
 #include "fvOptions.H"
-#include "fixedFluxPressureFvPatchScalarField.H"
+#include "CorrectPhi.H"
 #include "lentMethod.H"
 
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
@@ -77,14 +73,16 @@ int main(int argc, char *argv[])
 
     pimpleControl pimple(mesh);
 
+    #include "createTimeControls.H"
     #include "initContinuityErrs.H"
     #include "createFields.H"
     #include "createMRF.H"
     #include "createFvOptions.H"
-
-    #include "createTimeControls.H"
-    #include "readTimeControls.H"
     #include "correctPhi.H"
+
+    turbulence->validate();
+
+    #include "readTimeControls.H"
     #include "CourantNo.H"
     #include "setInitialDeltaT.H"
 
@@ -150,11 +148,15 @@ int main(int argc, char *argv[])
 
         // Update the viscosity. 
         mixture.correct();
+
         // Update density field.
         rho == markerField*rho1 + (scalar(1) - markerField)*rho2;
+
         // The momentum flux is computed from MULES as  
         // rhoPhi = phiAlpha*(rho1 - rho2) + phi*rho2; 
         // However, LENT has no ability to compute the volumetric phase flux. 
+        // TODO: examine the impact of the momentum flux computation and devise
+        // more accurate approach if required (TT)
         rhoPhi == fvc::interpolate(rho) * phi; 
 
         lent.reconstructFront(front, signedDistance, pointSignedDistance);
