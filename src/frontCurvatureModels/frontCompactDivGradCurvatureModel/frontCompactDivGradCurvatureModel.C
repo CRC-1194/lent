c320382fd130fae306e11838f93bb6b1da907e6f
Tobias Tolle <bt@lefou-familie.org>
2017-09-27 12:15:06 +0200

    Added a new correction approach for the divGrad curvature model. Needs testing.

--- src/frontCurvatureModels/frontCompactDivGradCurvatureModel/frontCompactDivGradCurvatureModel.C
+++ src/frontCurvatureModels/frontCompactDivGradCurvatureModel/frontCompactDivGradCurvatureModel.C
@@ -60,6 +60,9 @@ Description
 
 #include "frontCompactDivGradCurvatureModel.H"
 
+#include <algorithm>
+#include <iterator>
+
 #include "addToRunTimeSelectionTable.H"
 #include "triSurfaceFields.H"
 
@@ -72,6 +75,130 @@ namespace FrontTracking {
 
     defineTypeNameAndDebug(frontCompactDivGradCurvatureModel, 0);
     addToRunTimeSelectionTable(frontCurvatureModel, frontCompactDivGradCurvatureModel, Dictionary);
+
+
+// * * * * * * * * * * * * *  Private member functions * * * * * * * * * * * //
+void frontCompactDivGradCurvatureModel::applySphereCorrection(
+    const fvMesh& mesh,
+    const triSurfaceFront& front,
+    const volScalarField& signedDistance,
+    volScalarField& curvature
+) const
+{
+    const auto& communication = mesh.lookupObject<lentCommunication>(
+                                    lentCommunication::registeredName(front, mesh)
+                                ); 
+    // Only used as a list of all cells intersected by the interface
+    const auto& interfaceCells = communication.interfaceCellToTriangles();
+
+    for (const auto& p : interfaceCells)
+    {
+        const auto& temp = curvature[p.first];
+        curvature[p.first] = sign(temp) * 2.0 /
+                               (2.0/mag(temp) - signedDistance[p.first]);
+    }
+}
+
+void frontCompactDivGradCurvatureModel::applyInterpolationCorrection(
+    const fvMesh& mesh,
+    const triSurfaceFront& front,
+    const volScalarField& signedDistance,
+    volScalarField& curvature
+) const
+{
+    const auto& communication = mesh.lookupObject<lentCommunication>(
+                                    lentCommunication::registeredName(front, mesh)
+                                ); 
+    // Only used as a list of all cells intersected by the interface
+    const auto& interfaceCells = communication.interfaceCellToTriangles();
+    const auto& cellsTriangleNearest = communication.cellsTriangleNearest();
+    const auto& C = mesh.C();
+
+    for (const auto& p : interfaceCells)
+    {
+        const auto& cellLabel = p.first;
+
+        // Idea: find neighbour cell for which the connection of the
+        // two cell centres deviates the least from the interface normal
+        //
+        // Note: it is not sufficient to look only at the cells connected by a
+        // face
+        scalar maxCosine = 0.0;
+        label interpolationCellLabel = -1;
+        auto normal = cellsTriangleNearest[cellLabel].hitPoint() - C[cellLabel];
+        normal /= (mag(normal) + SMALL);
+
+        // No need to correct if cell centre is located very close to the
+        // interface
+        if (mag(signedDistance[cellLabel]) > SMALL)
+        {
+            auto extendedNeighbourCells = findNeighbourCells(mesh, cellLabel);
+
+            for (const auto& neighbourCellLabel : extendedNeighbourCells)
+            {
+                if (signedDistance[cellLabel]*signedDistance[neighbourCellLabel] > 0.0)
+                {
+                    continue;
+                }
+
+                auto cc = C[cellLabel] - C[neighbourCellLabel];
+                cc /= mag(cc);
+
+                auto cosine = mag(cc & normal);
+
+                if (cosine > maxCosine)
+                {
+                    maxCosine = cosine;
+                    interpolationCellLabel = neighbourCellLabel;
+                }
+            }
+
+            curvature[cellLabel] = 
+                (
+                 curvature[cellLabel]*mag(signedDistance[interpolationCellLabel])
+                 +
+                 curvature[interpolationCellLabel]*mag(signedDistance[cellLabel])
+                )
+                /
+                (mag(signedDistance[cellLabel]) + mag(signedDistance[interpolationCellLabel]));
+        }
+    }
+}
+
+// TODO: this probably needs some serious optimization
+std::vector<label> frontCompactDivGradCurvatureModel::findNeighbourCells(
+    const fvMesh& mesh,
+    const label& cellLabel
+) const
+{
+    std::vector<label> neighbourCells{};
+
+    const auto& vertexLabels = mesh.cellPoints()[cellLabel];
+    const auto& vertexToCell = mesh.pointCells();
+
+    forAll(vertexLabels, I)
+    {
+        const auto& connectedCells = vertexToCell[vertexLabels[I]];
+
+        forAll(connectedCells, K)
+        {
+            if (connectedCells[K] != cellLabel)
+            {
+                neighbourCells.push_back(connectedCells[K]);
+            }
+        }
+    }
+
+    // Remove duplicate entries
+    std::sort(neighbourCells.begin(), neighbourCells.end());
+    
+    std::vector<label>::iterator newEnd = 
+                std::unique(neighbourCells.begin(), neighbourCells.end());
+
+    neighbourCells.resize(std::distance(neighbourCells.begin(), newEnd));
+
+    return neighbourCells;
+}
 
 // * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
 
@@ -118,20 +245,37 @@ tmp<volScalarField> frontCompactDivGradCurvatureModel::cellCurvature(
     const volScalarField& curvatureInputField = 
         mesh.lookupObject<volScalarField>(curvatureInputFieldName()); 
 
+    // Apply corrections
+    if (distanceCorrection_ == "sphere")
+    {
+        applySphereCorrection(mesh, frontMesh, curvatureInputField, cellCurvatureField);
+    }
+    else if (distanceCorrection_ == "interpolation")
+    {
+        applyInterpolationCorrection(mesh, frontMesh, curvatureInputField, cellCurvatureField);
+    }
+    else if (distanceCorrection_ == "off")
+    {
+    }
+    else
+    {
+        FatalErrorIn (
+            "frontCompactDivGradCurvatureModel::cellCurvature(front, mesh)"
+        )   << "Unknown distance correction "
+            << distanceCorrection_
+            << " for the keyword 'distanceCorrection'.\n"
+            << "Valid options are 'sphere', 'interpolation' or 'off'."
+            << exit(FatalError);
+    }
+
+    // Write to temporary front field to communicate curvature back to
+    // Eulerian mesh
     forAll(triangleToCell, index)
     {
-        auto temp = cellCurvatureField[triangleToCell[index]];
-
-        if (distanceCorrection_ == "on")
-        {
-            curvatureBuffer[index] = sign(temp) * 2.0 / (2.0/mag(temp)
-                                            - curvatureInputField[triangleToCell[index]]);
-        }
-        else
-        {
-            curvatureBuffer[index] = temp;
-        }
+        curvatureBuffer[index] = cellCurvatureField[triangleToCell[index]];
     }
+
+    // Propagate compact curvature to narrow band cells
     cellCurvatureField *= 0.0;
 
     const auto& cellToTriangle = communication.cellsTriangleNearest();
