a97e1fca08dec8ef0f25804353ae4eb6da122cbe
Tomislav Maric <tomislav.maric@gmx.com>
2019-03-19 16:29:18 +0100

    rbf gradient stabilization

--- applications/testing/lentTestRbfInterpolation/lentTestRbfInterpolation.C
+++ applications/testing/lentTestRbfInterpolation/lentTestRbfInterpolation.C
@@ -45,48 +45,60 @@ Author
 #include <fstream>
 #include "gtest.h"
 
+using namespace Foam;
+using namespace RBF;
+
+#include "lentTestRbfInterpolationHelpers.H"
+
 using realVector = Eigen::VectorXd; 
 using point = Eigen::Vector3d;  
 using pointVector = std::vector<point>; 
 
+// Random number generators.
 static std::random_device rd;  
 static std::mt19937 gen(rd()); 
 static std::uniform_real_distribution<> dis(0, 1.0);
 static std::uniform_int_distribution<> dissign(0, 1);
-std::array<int, 2> signs = {-1, +1}; 
+
+static const std::array<int, 2> signs = {-1, +1}; 
+
+// Points of the BCC RBF cubic stencil
+static const pointVector bccPoints = {
+        point{0,0,0}, // Cube corner points.
+        point{1,0,0},
+        point{1,1,0},
+        point{0,1,0},
+        point{0,0,1},
+        point{1,0,1},
+        point{1,1,1},
+        point{0,1,1},
+        point{0.5,0.5,0.5} // Cube centroid.
+    };
+
+// Points of the BCC_FVM RBF cubic stencil
+static const pointVector bccFvmPoints = {
+        point{0,0,0}, // Cube corner points.
+        point{1,0,0},
+        point{1,1,0},
+        point{0,1,0},
+        point{0,0,1},
+        point{1,0,1},
+        point{1,1,1},
+        point{0,1,1},
+        point{0.5,0.5,0.5}, // Cube centroid.
+        point{-0.5,0.5,0.5}, // Neighbor cube centroids.
+        point{0.5,-0.5,0.5},
+        point{1.5,0.5,0.5},
+        point{0.5,1.5,0.5},
+        point{0.5,0.5,-0.5},
+        point{0.5,0.5,1.5},
+    };
 
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
 
-// Extract these into functions
-template<typename Points, typename Distribution, typename Generator>
-void seedPoints(
-    Points& points, 
-    Distribution& dis, 
-    Generator& gen
-)
-{
-    for (auto& point : points)
-    {
-        point[0] = dis(gen);  
-        point[1] = dis(gen);  
-        point[2] = dis(gen);  
-    }
-};
 
-template<typename Values, typename Points>
-void setValuesXnYnZn(Values& values, Points const& points, double N)
-{
-    const long unsigned int Npoints = points.size();
-    const long unsigned int Nvalues = values.size(); 
-    EXPECT_EQ(Npoints, Nvalues);
-    for (long unsigned int i = 0; i < Npoints; ++i)
-        values[i] = pow(points[i][0], N) 
-            + pow(points[i][1], N) + pow(points[i][2], N);
-};
-
-using namespace Foam;
-using namespace RBF;
-
+// Tests the emtpy and component constructors and solution member functions
+// of the rbfInterpolationEigen<Kernel> class template.
 template<typename RbfTuple, typename Points, int N = 0> 
 void test_rbf_interface(Points const& nodalPoints)
 {
@@ -100,8 +112,6 @@ void test_rbf_interface(Points const& nodalPoints)
     unsigned long int Nvalues = nodalValues.size(); 
 
     ASSERT_EQ(Npoints, Nvalues);
-
-    // TEST empty and full (component constructor)
 
     // Construct empty. 
     rbfInterpolation rbfEmpty; 
@@ -185,42 +195,21 @@ TEST(RBF_EIGEN, CLASS_INTERFACE)
     test_rbf_interface<rbfTuple>(nodalPoints); 
 }
 
-static const pointVector bccPoints = {
-        point{0,0,0}, // Cube corner points.
-        point{1,0,0},
-        point{1,1,0},
-        point{0,1,0},
-        point{0,0,1},
-        point{1,0,1},
-        point{1,1,1},
-        point{0,1,1},
-        point{0.5,0.5,0.5} // Cube centroid.
-    };
-
-static const pointVector bccFvmPoints = {
-        point{0,0,0}, // Cube corner points.
-        point{1,0,0},
-        point{1,1,0},
-        point{0,1,0},
-        point{0,0,1},
-        point{1,0,1},
-        point{1,1,1},
-        point{0,1,1},
-        point{0.5,0.5,0.5}, // Cube centroid.
-        point{-0.5,0.5,0.5}, // Neighbor cube centroids.
-        point{0.5,-0.5,0.5},
-        point{1.5,0.5,0.5},
-        point{0.5,1.5,0.5},
-        point{0.5,0.5,-0.5},
-        point{0.5,0.5,1.5},
-    };
-
-template<typename RbfTuple, typename Points, typename RealVector, typename Plane, int N = 0> 
+// Tests the Linf value errors for nodalValues at nodalPoints and testValues
+// at testPoints (tolerance-based).
+template
+<
+    typename RbfTuple, 
+    typename Points, 
+    typename RealVector, 
+    typename Surface, 
+    int N = 0
+> 
 void test_rbf_values_and_grads(
     Points const& nodalPoints, 
     RealVector const& nodalValues,
     Points const& testPoints, 
-    Plane const& testPlane
+    Surface const& surf 
 )
 {
     using rbfKernelType = std::tuple_element_t<N, RbfTuple>;
@@ -242,148 +231,77 @@ void test_rbf_values_and_grads(
     for (decltype(nodalPoints.size()) pointI = 0; pointI < nodalPoints.size(); ++pointI)
     {
         const auto rbfNodalValue = rbf.value(nodalPoints[pointI], nodalPoints);
-        const auto testPlaneValue = testPlane.value(nodalPoints[pointI]);
-        const auto nodalError = std::abs(rbfNodalValue - testPlaneValue); 
+        const auto surfValue = surf.value(nodalPoints[pointI]);
+        const auto nodalError = std::abs(rbfNodalValue - surfValue); 
 
         ASSERT_LE(nodalError, 8 * EPS)
             << rbfKernelType::name() << std::endl
-            << "Plane point = " << testPlane.point() << std::endl
-            << "Plane normal = " << testPlane.normal() << std::endl;
+            << "Surface = " << surf << std::endl;
 
-        Eigen::Vector3d rbfNodalGrad = rbf.grad(nodalPoints[pointI], nodalPoints); 
-        Eigen::Vector3d rbfNodalErr = rbfNodalGrad - testPlane.normal(); 
+        Eigen::Vector3d rbfNodalGrad = 
+            rbf.template grad<Eigen::Vector3d>(nodalPoints[pointI], nodalPoints); 
+        Eigen::Vector3d surfGrad = 
+            surf.template grad<Eigen::Vector3d>(nodalPoints[pointI]); 
+        Eigen::Vector3d rbfNodalErr = rbfNodalGrad - surfGrad;
         const auto rbfNodalGradErrMag = std::sqrt(rbfNodalErr.dot(rbfNodalErr));
-
         ASSERT_LE(rbfNodalGradErrMag, 128 * EPS) // Evaluation of RBF gradients near nodal points is unstable.
             << rbfKernelType::name() << std::endl
-            << "Plane point = " << testPlane.point() << std::endl
-            << "Plane normal = " << testPlane.normal() << std::endl
+            << "Surface = " << surf << std::endl
             << "Rbf grad = " << rbfNodalGrad << std::endl
-            << "Error = " << rbfNodalGradErrMag << std::endl;
+            << "Surf grad = " << surfGrad << std::endl
+            << "Error = " << rbfNodalGradErrMag << std::endl
+            << "Point = " << nodalPoints[pointI] << std::endl;
     }
 
-    // Values at test points.
-    for (decltype(testPoints.size()) pointI = 0; pointI < testPoints.size(); ++pointI)
-    {
-        const auto rbfValue = rbf.value(testPoints[pointI], nodalPoints);
-        const auto planeValue = testPlane.value(testPoints[pointI]);
-        const auto testError = std::abs(rbfValue - planeValue); 
-        ASSERT_LE(testError, 8 * EPS)
-            << rbfKernelType::name() << std::endl;
-    }
-
-    // Gradients at test points. 
+    // Gradients and values at test points. 
     for (decltype(testPoints.size()) pointI = 0; pointI < testPoints.size(); ++pointI)
     {
         const auto rbfTestValue = rbf.value(testPoints[pointI], nodalPoints);
-        const auto testPlaneValue = testPlane.value(testPoints[pointI]);
-        const auto testError = std::abs(rbfTestValue - testPlaneValue); 
+        const auto surfValue = surf.value(testPoints[pointI]);
+        const auto testError = std::abs(rbfTestValue - surfValue); 
 
         ASSERT_LE(testError, 8 * EPS)
             << rbfKernelType::name() << std::endl
-            << "Plane point = " << testPlane.point() << std::endl
-            << "Plane normal = " << testPlane.normal() << std::endl;
+            << "Surface = " << surf << std::endl;
 
         Eigen::Vector3d rbfTestGrad = rbf.grad(testPoints[pointI], nodalPoints); 
-        Eigen::Vector3d rbfTestErr = rbfTestGrad - testPlane.normal(); 
+        Eigen::Vector3d surfGrad = 
+            surf.template grad<Eigen::Vector3d>(testPoints[pointI]); 
+        Eigen::Vector3d rbfTestErr = rbfTestGrad - surfGrad;
         const auto rbfTestGradErrMag = std::sqrt(rbfTestErr.dot(rbfTestErr));
 
         ASSERT_LE(rbfTestGradErrMag, 64 * EPS) 
-            << rbfKernelType::name() << std::endl
-            << "Plane point = " << testPlane.point() << std::endl
-            << "Plane normal = " << testPlane.normal() << std::endl
+            << "Surface = " << surf << std::endl
             << "Rbf grad = " << rbfTestGrad << std::endl
-            << "Error = " << rbfTestGradErrMag << std::endl;
+            << "Surf grad = " << surfGrad << std::endl
+            << "Error = " << rbfTestGradErrMag << std::endl
+            << "Point = " << testPoints[pointI] << std::endl;
     }
  
     // Loop over RBF kernels.
     if constexpr (N + 1 < std::tuple_size_v<RbfTuple>) 
-        test_rbf_values_and_grads<RbfTuple, Points, RealVector, Plane, N + 1>(
+        test_rbf_values_and_grads<RbfTuple, Points, RealVector, Surface, N + 1>(
                 nodalPoints, 
                 nodalValues,
                 testPoints, 
-                testPlane
+                surf 
             );
 }
 
-template<typename Point, typename DIS, typename GEN>
-Point rand_point(DIS& dis, GEN& gen)
+TEST(RBF_EIGEN, PLANE_STENCILS)
 {
-    Point pt; 
-    pt[0] = dis(gen);
-    pt[1] = dis(gen);
-    pt[2] = dis(gen);
-    return pt;
-}
-
-class plane
-{
-    Eigen::Vector3d point_; 
-    Eigen::Vector3d normal_;
-
-    public: 
-
-        plane()
-            :
-                point_(), 
-                normal_()
-        {}
-
-        template<typename DIS, typename DISSIGN, typename GEN, typename SIGNS>
-        void randomize(DIS& dis, DISSIGN& dissign, GEN& gen, SIGNS const& signs)
-        {
-            point_ = rand_point<Eigen::Vector3d>(dis, gen);
-            normal_ = rand_point<Eigen::Vector3d>(dis, gen);
-            normal_[0] = signs[dissign(gen)] * normal_[0];
-            normal_[1] = signs[dissign(gen)] * normal_[1];
-            normal_[2] = signs[dissign(gen)] * normal_[2];
-            normal_.normalize();  
-        }
-
-        template<typename Vector>
-        decltype(auto) value(Vector&& evalPoint) const
-        {
-            return (evalPoint - point_).dot(normal_); 
-        }
-
-        decltype(auto) normal() const
-        {
-            return normal_;
-        }
-
-        decltype(auto) point() const
-        {
-            return point_;
-        }
-};
-
-template<typename Values, typename Points, typename Plane> 
-void set_plane_values(
-    Values& vals, 
-    Points const& points, 
-    Plane const& plane
-)
-{
-    for (decltype(points.size()) pI = 0; pI < points.size(); ++pI)
-        vals[pI] = plane.value(points[pI]);
-}
-
-TEST(RBF_EIGEN, STENCILS)
-{
-
-    pointVector testPoints(1000); 
+    // Random evaluation points.
+    static pointVector testPoints(1000); 
     seedPoints(testPoints, dis, gen); 
 
     for (int testI = 0; testI < 1000; ++testI)
     {
-        plane testPlane; 
+        eigenPlane testPlane; 
         testPlane.randomize(dis,dissign,gen,signs); 
 
-        // BCC Stencil
-        
         // BCC Plane test
         realVector bccPlaneValues (bccPoints.size()); 
-        set_plane_values(bccPlaneValues, bccPoints, testPlane);
+        set_surface_values(bccPlaneValues, bccPoints, testPlane);
 
         test_rbf_values_and_grads<rbfTuple>(
             bccPoints, 
@@ -391,14 +309,10 @@ TEST(RBF_EIGEN, STENCILS)
             testPoints, 
             testPlane
         ); 
-
-        // END BCC Stencil
-
-        // BCC_FVM Stencil
         
         // BCC_FVM plane test
         realVector bccFvmPlaneValues (bccFvmPoints.size()); 
-        set_plane_values(bccFvmPlaneValues, bccFvmPoints, testPlane);
+        set_surface_values(bccFvmPlaneValues, bccFvmPoints, testPlane);
 
         test_rbf_values_and_grads<rbfTuple>(
             bccFvmPoints, 
@@ -406,11 +320,39 @@ TEST(RBF_EIGEN, STENCILS)
             testPoints, 
             testPlane
         ); 
-        
-        // END BCC_FVM Stencil
     }
 }
 
+TEST(RBF_EIGEN, SPHERE_STENCILS)
+{
+    // Random evaluation points.
+    static pointVector testPoints(1000); 
+    seedPoints(testPoints, dis, gen); 
+
+    eigenSphere testSphere(Eigen::Vector3d(0,0,0), 0.50173); 
+
+    // BCC Sphere test
+    realVector bccSphereValues(bccPoints.size()); 
+    set_surface_values(bccSphereValues, bccPoints, testSphere);
+
+    test_rbf_values_and_grads<rbfTuple>(
+        bccPoints, 
+        bccSphereValues, 
+        testPoints, 
+        testSphere 
+    ); 
+
+    // BCC_FVM Sphere test
+    realVector bccFvmSphereValues(bccFvmPoints.size()); 
+    set_surface_values(bccFvmSphereValues, bccFvmPoints, testSphere);
+
+    test_rbf_values_and_grads<rbfTuple>(
+        bccFvmPoints, 
+        bccFvmSphereValues, 
+        testPoints, 
+        testSphere 
+    ); 
+}
 int mainArgc;
 char** mainArgv;
 
