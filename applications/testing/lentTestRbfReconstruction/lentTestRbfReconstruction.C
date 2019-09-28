29d80bb77ccb90e3c65afe6e20dd51ada7a7486d
Tomislav Maric <tomislav.maric@gmx.com>
2019-09-28 11:15:08 +0200

    RBF reconstruction: error output

---
+++ applications/testing/lentTestRbfReconstruction/lentTestRbfReconstruction.C
@@ -0,0 +1,97 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     | 
+    \\  /    A nd           | Copyright (C) 2018 Tomislav Maric, TU Darmstadt
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software; you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by the
+    Free Software Foundation; either version 2 of the License, or (at your
+    option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM; if not, write to the Free Software Foundation,
+    Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+
+Author
+    Tomislav Maric maric@mma.tu-darmstadt.de
+
+Description
+    Test application for the RBF interpolation on an FV mesh.
+
+    Tests
+        - rbfCellsInterpolationEigen: RBF interpolation over cell stencils
+
+\*---------------------------------------------------------------------------*/
+
+#include "fvCFD.H"
+#include "pointFields.H"
+
+#include "rbFunctions.H"
+#include "rbfCellsInterpolationEigen.H"
+#include "analyticalEllipsoid.H"
+#include "sphereHypersurface.H"
+#include "ellipsoidHypersurface.H"
+#include "rbfIsoPointCalculator.H"
+#include "isoPointCalculator.H"
+
+// Time measurement.
+#include <chrono>
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+using namespace Foam;
+using namespace RBF;
+using namespace FrontTracking;
+
+#include "lentTestRbfReconstructionHelpers.H"
+
+int main(int argc, char **argv)
+{
+    #include "setRootCase.H"
+    #include "createTime.H"
+    #include "createMesh.H"
+
+    if(mesh.nCells() > 64 * 64 * 64) 
+        WarningIn(__PRETTY_FUNCTION__) 
+            << "Request for memory may be too large. " << endl
+            << "Memory usage of the RBF factorization is NxN x nCells x 64 bits, " << endl
+            << "where N is the number of points in the RBF stencil (hex cells: 9 for BCC, 15 for BCC_FVM).\n";
+
+    // Initialize the surface test fields.
+    ellipsoidHypersurface ellipsoid (1/3., 1/2., 2/3.);
+    surfaceTestFields ellipsoidFields(mesh, ellipsoid, "Ellipsoid");
+    sphereHypersurface sphere(point(0., 0., 0.), 0.5);
+    surfaceTestFields sphereFields(mesh, sphere, "Sphere");
+
+    // Test linear point reconstruction 
+    OFstream linErrorFile("linearPositioningErrors.csv"); 
+    linErrorFile << "SURFACE,LINF_EDGE,L1_EDGE,L2_EDGE, LINF_CELL, L1_CELL, L2_CELL, CPU_TIME_SECONDS" << endl; 
+    testLinearIsoPoints(sphere, sphereFields, linErrorFile); 
+    testLinearIsoPoints(ellipsoid, ellipsoidFields, linErrorFile); 
+
+    // Test RBF reconstruction 
+    OFstream rbfErrorFile("rbfPositioningErrors.csv"); 
+    rbfErrorFile << "RBF,STENCIL,SURFACE,LINF_CELL,POINT_CORR_CPU_TIME_SEC,FACTOR_CPU_TIME_SEC,SOL_CPU_TIME_SEC" << endl; 
+    testRbfReconstruction<rbfTuple>(sphere, sphereFields, rbfErrorFile);
+    testRbfReconstruction<rbfTuple>(ellipsoid, ellipsoidFields, rbfErrorFile);
+
+    ellipsoidFields.writeValueFields();
+    sphereFields.writeValueFields();
+
+    Info<< nl;
+    Info<< "End\n" << endl;
+
+    return 0;
+}
+
+// ************************************************************************* //
