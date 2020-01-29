ebc400eaf194293e61f9e6167e2a3446eceae56d
Tomislav Maric <tomislav.maric@gmx.com>
2020-01-29 16:50:50 +0100

    notebookPlotting: line formatting

--- cases/scripts/modules/notebookPlotting.py
+++ cases/scripts/modules/notebookPlotting.py
@@ -8,17 +8,16 @@ from matplotlib.lines import Line2D
 from matplotlib import rcParams
 import glob
 
-# Set plotting style parameters
-rcParams['figure.dpi'] = 300
-lStyles = list(lines.lineStyles.keys())
-mStyles = list(Line2D.markers.keys())
-
 import dataAgglomeration as datglom
 
 def plot_study(xy_filter=lambda y : True, paramFile="", dirExample="", dataFile="", colDict = {}):
     """Agglomerate all dataFile files in each directory whose name contains parameterFile, 
     and plot xColName column and yColName column, label the graph with the index of the 
     variation and the parameters of the variation in the plot legend."""
+
+    # Set plotting style parameters
+    lStyles = list(lines.lineStyles.keys())
+    mStyles = list(Line2D.markers.keys())
     
     # X and Y-axis column names and diagram symbols.
     xColName = colDict["x"]
@@ -31,13 +30,13 @@ def plot_study(xy_filter=lambda y : True, paramFile="", dirExample="", dataFile=
     indexLevels = [i for i,name in enumerate(studyDframe.index.names) if 'step' not in name]
     collector = agglomerator.data_collector
     variations = collector.valid_variations
-    variationI = 0 
     fig, ax = plt.subplots()
     ax.set_xlabel(xColSymb)
     ax.set_ylabel(yColSymb)
     
-    # FIXME: Directory name contains an prefix - fetch the template copy?
     ax.set_title("%s" % dirExample.split('.')[0] + "-" + dirExample.split('_')[-1]) 
+    # FIXME: Directory name contains an prefix - fetch the template copy?
+    variationI = 0 
     for paramLine, subDframe in studyDframe.groupby(level=indexLevels):
         xCol = subDframe[xColName] 
         yCol = subDframe[yColName]
@@ -54,8 +53,11 @@ def plot_study(xy_filter=lambda y : True, paramFile="", dirExample="", dataFile=
             paramString = indexName + "=%d " % paramLine 
             
         if (xy_filter(xCol, yCol)):
-            ax.plot(xCol, yCol, label="variation=%04d " % variationI + paramString)
+            ax.plot(xCol, yCol, label="variation=%04d " % variationI + paramString, 
+                    marker=mStyles[variationI % len(mStyles)], 
+                    linestyle=lStyles[variationI % len(lStyles)])
         variationI = variationI + 1
 
-    ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.15),fancybox=True, shadow=True, ncol=1)
+    ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.15),fancybox=True, shadow=True, ncol=1) 
+              
     plt.show()
