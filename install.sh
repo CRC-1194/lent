02fa5cfb01606be4496802d819415bac27d1d569
Tomislav Maric <tomislav.maric@gmx.com>
2020-02-21 12:18:46 +0100

    Compiles on Lichtenberg

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 cd ${0%/*} || exit 1    # run from this directory
 
-
-mkdir build && cd build
+rm -rf build && mkdir build && cd build
 cmake -DCMAKE_INSTALL_PREFIX=./ -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=on ..
 make && make install
