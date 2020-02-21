8e648b4d3a051d360d6f0adecd7141acda08ac66
Tomislav Maric <tomislav.maric@gmx.com>
2020-02-21 15:07:44 +0100

    CMake : find Eigen3 using CMAKE_PREFIX_PATH

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@ cd ${0%/*} || exit 1    # run from this directory
 
 rm -rf build && mkdir build && cd build
 cmake -DCMAKE_INSTALL_PREFIX=./ -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=on ..
-make && make install
+make -j 4 && make install
