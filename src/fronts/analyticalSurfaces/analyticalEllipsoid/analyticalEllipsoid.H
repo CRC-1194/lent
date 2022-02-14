473b01ee9b4bbc78ae8b2e087a72507ef1e0ceb5
Tobias Tolle <bt@lefou-familie.org>
2022-02-14 19:50:00 +0100

    src/fronts/analyticalSurfaces

--- src/fronts/analyticalSurfaces/analyticalEllipsoid/analyticalEllipsoid.H
+++ src/fronts/analyticalSurfaces/analyticalEllipsoid/analyticalEllipsoid.H
@@ -1,17 +1,17 @@
 /*---------------------------------------------------------------------------*\
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
-   \\    /   O peration     |
-    \\  /    A nd           | Copyright (C) 2018 Tobias Tolle, TU Darmstadt 
+   \\    /   O peration     | 
+    \\  /    A nd           | 
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
     This file is part of OpenFOAM.
 
-    OpenFOAM is free software: you can redistribute it and/or modify it
-    under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+    OpenFOAM is free software; you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by the
+    Free Software Foundation; either version 2 of the License, or (at your
+    option) any later version.
 
     OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
     ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
@@ -19,23 +19,32 @@ License
     for more details.
 
     You should have received a copy of the GNU General Public License
-    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+    along with OpenFOAM; if not, write to the Free Software Foundation,
+    Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
 
 Class
-    Foam::analyticalEllipsoid
+    Foam::FrontTracking:analyticalEllipsoid
 
 SourceFiles
     analyticalEllipsoid.C
 
-Author
-    Tobias Tolle   tolle@csi.tu-darmstadt.de
+Authors
+    Tobias Tolle (tolle@mma.tu-darmstadt.de)
 
 Description
     Specialization of the analyticalSurface class for an ellipsoid.
 
-    See https://www.geometrictools.com/Documentation/DistancePointEllipseEllipsoid.pdf
-    for information on how to find the minimal distance between a point
-    and the ellipsoid.
+Affiliations:
+    Mathematical Modeling and Analysis Institute, Mathematics Department, 
+    TU Darmstadt, Germany
+
+Funding:
+    German Research Foundation (DFG) - Project-ID 265191195 - SFB 1194
+
+    German Research Foundation (DFG) - Project-ID MA 8465/1-1, 
+    Initiation of International Collaboration 
+    "Hybrid Level Set / Front Tracking methods for simulating 
+    multiphase flows in geometrically complex systems"
 
 \*---------------------------------------------------------------------------*/
 
