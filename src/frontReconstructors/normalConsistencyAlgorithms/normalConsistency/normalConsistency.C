ef873a0a5c02730f098c41dd1d48324649a70cd4
Tomislav Maric <tomislav.maric@gmx.com>
2022-02-14 18:47:05 +0100

    src/normalConsitencyAlgorithms

--- src/frontReconstructors/normalConsistencyAlgorithms/normalConsistency/normalConsistency.C
+++ src/frontReconstructors/normalConsistencyAlgorithms/normalConsistency/normalConsistency.C
@@ -1,8 +1,8 @@
 /*---------------------------------------------------------------------------*\
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
-   \\    /   O peration     | Version:  2.2.x                               
-    \\  /    A nd           | Copyright held by original author
+   \\    /   O peration     | 
+    \\  /    A nd           | 
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
@@ -23,37 +23,27 @@ License
     Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
 
 Class
-    Foam::diffuseInterfaceProperties
+    Foam::distanceNormalConsistency
 
 SourceFiles
-    diffuseInterfaceProperties.C
+    distanceNormalConsistency.C
 
-Author
-    Tomislav Maric maric@csi.tu-darmstadt.de, tomislav@sourceflux.de
-    Mathematical Modeling and Analysis
-    Center of Smart Interfaces, TU Darmstadt
+Authors
+    Tomislav Maric (maric@mma.tu-darmstadt.de)
 
 Description
-    
-    You may refer to this software as :
-    //- full bibliographic data to be provided
 
-    This code has been developed by :
-        Tomislav Maric maric@csi.tu-darmstadt.de, tomislav@sourceflux.de (main developer)
-    under the project supervision of :
-        Holger Marschall <marschall@csi.tu-darmstadt.de> (group leader).
-    
-    Method Development and Intellectual Property :
-    	Tomislav Maric maric@csi.tu-darmstadt.de, tomislav@sourceflux.de
-    	Holger Marschall <marschall@csi.tu-darmstadt.de>
-    	Dieter Bothe <bothe@csi.tu-darmstadt.de>
+Affiliations:
+    Mathematical Modeling and Analysis Institute, Mathematics Department, 
+    TU Darmstadt, Germany
 
-        Mathematical Modeling and Analysis
-        Center of Smart Interfaces
-        Technische Universitaet Darmstadt
-       
-    If you use this software for your scientific work or your publications,
-    please don't forget to acknowledge explicitly the use of it.
+Funding:
+    German Research Foundation (DFG) - Project-ID 265191195 - SFB 1194
+
+    German Research Foundation (DFG) - Project-ID MA 8465/1-1, 
+    Initiation of International Collaboration 
+    "Hybrid Level Set / Front Tracking methods for simulating 
+    multiphase flows in geometrically complex systems"
 
 \*---------------------------------------------------------------------------*/
 
