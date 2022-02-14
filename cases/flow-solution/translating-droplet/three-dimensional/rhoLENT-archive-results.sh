0d6cdfd9c4acdad49fa417b9dc8a211f6fb373f3
Tomislav Maric <tomislav.maric@gmx.com>
2022-02-14 15:57:14 +0100

    3D translating droplet: data archival fixed

--- cases/flow-solution/translating-droplet/three-dimensional/rhoLENT-archive-results.sh
+++ cases/flow-solution/translating-droplet/three-dimensional/rhoLENT-archive-results.sh
@@ -10,8 +10,9 @@ mkdir rhoLENT-data
 cp rhoLENT-data.md rhoLENT-data
 cp --parents $(find . -name stationaryDropletResults.csv) rhoLENT-data/
 cp ../../../scripts/modules/*.py rhoLENT-data/
-./rhoLENT-document-variations.sh
-cp *.parameter *.variations rhoLENT-data/
+
 ./rhoLENT-visualize-studies.sh
+
+cp *.parameter *.variations rhoLENT-data/
 cp *.ipynb *.html *.csv *.json rhoLENT-data/
 tar czf rhoLENT-data.tgz rhoLENT-data
