8877b905b3c0ccfb76612314b56612c45ca236f9
Tomislav Maric <tomislav.maric@gmx.com>
2022-02-14 19:46:56 +0100

    src/curvatureModels

--- src/curvatureModels/exactCurvatureModels/exactCurvatureModel/exactCurvatureModel.C
+++ src/curvatureModels/exactCurvatureModels/exactCurvatureModel/exactCurvatureModel.C
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     |
-    \\  /    A nd           | Copyright (C) 2018 AUTHOR,AFFILIATION
+    \\  /    A nd           | 
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
@@ -20,6 +20,30 @@ License
 
     You should have received a copy of the GNU General Public License
     along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+Class
+    Foam::exactCurvatureModel
+
+SourceFiles
+    exactCurvatureModelI.H
+    exactCurvatureModel.C
+    exactCurvatureModelIO.C
+
+Authors:
+    Tobias Tolle   (tolle@mma.tu-darmstadt.de)
+ 
+Affiliations:
+    Mathematical Modeling and Analysis Institute, Mathematics Department, 
+    TU Darmstadt, Germany
+
+Funding:
+    German Research Foundation (DFG) - Project-ID 265191195 - SFB 1194
+
+    German Research Foundation (DFG) - Project-ID MA 8465/1-1, 
+    Initiation of International Collaboration 
+    "Hybrid Level Set / Front Tracking methods for simulating 
+    multiphase flows in geometrically complex systems"
+
 
 \*---------------------------------------------------------------------------*/
 
--- src/curvatureModels/frontBasedCurvatureModels/meyerCurvatureModel/meyerCurvatureModel.C
+++ src/curvatureModels/frontBasedCurvatureModels/meyerCurvatureModel/meyerCurvatureModel.C
@@ -1,8 +1,8 @@
 /*---------------------------------------------------------------------------*\
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
-   \\    /   O peration     | Version:  2.2.x                               
-    \\  /    A nd           | Copyright held by original author
+   \\    /   O peration     | 
+    \\  /    A nd           | 
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
@@ -28,34 +28,21 @@ Class
 SourceFiles
     meyerCurvatureModel.C
 
-Author
-    Tobias Tolle bt@lefou-familie.org
+Authors:
+    Tobias Tolle   (tolle@mma.tu-darmstadt.de)
+    Tomislav Maric (tolle@mma.tu-darmstadt.de)
+ 
+Affiliations:
+    Mathematical Modeling and Analysis Institute, Mathematics Department, 
+    TU Darmstadt, Germany
 
-Contributors
-    Tomislav Maric maric@csi.tu-darmstadt.de
+Funding:
+    German Research Foundation (DFG) - Project-ID 265191195 - SFB 1194
 
-Description
-    Interface for the front curvature models. 
-
-    You may refer to this software as :
-    //- full bibliographic data to be provided
-
-    This code has been developed by :
-        Tomislav Maric maric@csi.tu-darmstadt.de (main developer)
-    under the project supervision of :
-        Holger Marschall <marschall@csi.tu-darmstadt.de> (group leader).
-    
-    Method Development and Intellectual Property :
-    	Tomislav Maric maric@csi.tu-darmstadt.de
-    	Holger Marschall <marschall@csi.tu-darmstadt.de>
-    	Dieter Bothe <bothe@csi.tu-darmstadt.de>
-
-        Mathematical Modeling and Analysis
-        Center of Smart Interfaces
-        Technische Universitaet Darmstadt
-       
-    If you use this software for your scientific work or your publications,
-    please don't forget to acknowledge explicitly the use of it.
+    German Research Foundation (DFG) - Project-ID MA 8465/1-1, 
+    Initiation of International Collaboration 
+    "Hybrid Level Set / Front Tracking methods for simulating 
+    multiphase flows in geometrically complex systems"
 
 \*---------------------------------------------------------------------------*/
 
